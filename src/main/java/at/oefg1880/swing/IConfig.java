94e40806fa96576fe405553cb06d52f4744b09e9
Maximilian Schremser <schremse@algorithmics.com>
2012-02-10 14:15:37 +0100

    addedd license3j

--- src/main/java/at/oefg1880/swing/IConfig.java
+++ src/main/java/at/oefg1880/swing/IConfig.java
@@ -10,14 +10,19 @@ import java.awt.*;
  * To change this template use File | Settings | File Templates.
  */
 public interface IConfig {
-    // SAR GLOBAL CONFIGURATION PROPERTIES
-    public final static String NUM_ANSWERS = "numAnswers";
-    public final static String NUM_QUESTIONS = "numQuestions";
-    public final static String NUM_CATEGORIES = "numCategories";
-    public final static String NUM_SPREADS = "numSpreads";
+  // SAR GLOBAL CONFIGURATION PROPERTIES
+  public final static String NUM_ANSWERS = "numAnswers";
+  public final static String NUM_QUESTIONS = "numQuestions";
+  public final static String NUM_CATEGORIES = "numCategories";
+  public final static String NUM_SPREADS = "numSpreads";
+  public final static String LICENSE_FILE = "license.file";
+  public final static String PUBLIC_KEY = "public.key";
 
-    public final Color color_1 = new Color(255, 251, 234), color_2 = new Color(219, 216, 201);
-    public final Color selectedTextForeground = new Color(136, 181, 232), selectedListForeground = new Color(199, 215, 234), finishedColor = new Color(0, 100, 55);
-    public final static int VERTICAL = 0, HORIZONTAL = 1, DIAGONAL = 2, PLAIN_1 = 3, PLAIN_2 = 4;
-    public final static String OK = "OK", CANCEL = "CANCEL";
+  public final static String LICENSE_ISSUE_DATE = "issue-date";
+  public final static String LICENSE_VALID_DATE = "valid-date";
+
+  public final Color color_1 = new Color(255, 251, 234), color_2 = new Color(219, 216, 201);
+  public final Color selectedTextForeground = new Color(136, 181, 232), selectedListForeground = new Color(199, 215, 234), finishedColor = new Color(0, 100, 55);
+  public final static int VERTICAL = 0, HORIZONTAL = 1, DIAGONAL = 2, PLAIN_1 = 3, PLAIN_2 = 4;
+  public final static String OK = "OK", CANCEL = "CANCEL";
 }
