f19d283179b96b1a8cb1db017098c2186a29c4ed
Maximilian Schremser <schremse@algorithmics.com>
2012-02-10 14:33:10 +0100

    addedd license3j

---
+++ bin/decode.bat
@@ -0,0 +1,1 @@
+license3j.bat decode --license-file=license.out --keyring-file="C:\Users\schremse\AppData\Roaming\gnupg\pubring.gpg"
---
+++ bin/encode.bat
@@ -0,0 +1,5 @@
+rem $> gpg --gen-key
+rem 5
+rem 4096
+
+license3j.bat encode --license-file=license-dev.txt --keyring-file="C:\Users\schremse\AppData\Roaming\gnupg\secring.gpg" --key=TestToolDeveloperKey --password=weed4me2 --output=license.out
