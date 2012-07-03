18b23e6a9921d778177af0a329d31aeda35487d0
unknown <AT003053@IBM-LAGGRJMEKNN.(none)>
2012-07-03 13:34:50 +0200

    added online html help.

--- src/main/java/at/oefg1880/swing/frame/StartFrame.java
+++ src/main/java/at/oefg1880/swing/frame/StartFrame.java
@@ -62,8 +62,8 @@ public class StartFrame extends SheetableFrame implements ITexts, IConfig {
             }
         });
         FormLayout layout = new FormLayout(
-                "6dlu,pref,6dlu",
-                "6dlu,pref,6dlu,pref,6dlu");
+                "6dlu,pref",
+                "6dlu,pref,6dlu,pref");
         CellConstraints cc = new CellConstraints();
         GradientPanel panel = new GradientPanel();
         panel.setLayout(layout);
