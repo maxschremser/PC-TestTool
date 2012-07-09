e95b002bcc3736e815fd27bbfb46037b95c72ff6
unknown <AT003053@IBM-LAGGRJMEKNN.(none)>
2012-07-09 11:26:10 +0200

    added junit test to compare texts.properties with texts_en.properties

---
+++ src/test/java/at.oefg1880.swing/TestTextsProperties.java
@@ -0,0 +1,44 @@
+package at.oefg1880.swing;
+
+import junit.framework.TestCase;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: AT003053
+ * Date: 09.07.12
+ * Time: 10:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestTextsProperties extends TestCase {
+    Properties de_properties;
+    Properties en_properties;
+
+    public TestTextsProperties(String s) {
+        super(s);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        de_properties = new Properties();
+        de_properties.load(getClass().getClassLoader().getResourceAsStream("resources/texts.properties"));
+        en_properties = new Properties();
+        en_properties.load(getClass().getClassLoader().getResourceAsStream("resources/texts_en.properties"));
+    }
+
+    public void testConsistent() {
+        // each property that exists in de_properties must exist in en_properties
+        String sError = "";
+
+        Set<Object> keys = de_properties.keySet();
+        for (Object key : keys) {
+            String sKey = key.toString();
+//            assertTrue ("Property does not exist in text_en: " + sKey, en_properties.getProperty(sKey) != null);
+            if (en_properties.getProperty(sKey) == null) {
+                sError += "Property Missing: " + sKey + "\r\n";
+            }
+        }
+        assertTrue("Properties Missing: \r\n" + sError, sError.length() == 0);
+    }
+}
